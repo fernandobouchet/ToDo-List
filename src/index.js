e1c69973fc337b3473443b1f5afde797a31f7bd5
fernandobouchet <fernandobouchet@gmail.com>
2022-01-09 23:14:41 -0300

    Partiatly added today, this week filters. And added date-fns to filter dates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { format, isThisWeek, parseISO } from "date-fns";
 
 class Todo {
   constructor(title, description, dueDate, priority) {
@@ -36,6 +37,8 @@ class Lists {
 }
 
 const list = new Lists();
+const listToday = new Lists();
+const listWeek = new Lists();
 
 function createToDo() {
   const title = document.querySelector("#title").value;
@@ -46,10 +49,9 @@ function createToDo() {
   return newTodo;
 }
 
-function refreshList() {
-  removeList();
-  list.get().forEach((element) => {
-    addToList(element);
+function refreshList(listName, div) {
+  listName.get().forEach((element) => {
+    addToList(element, div);
   });
 }
 
@@ -62,7 +64,7 @@ function removeList() {
 
 const dom = document.getElementById("lists-content");
 
-function addToList(todo) {
+function addToList(todo, div) {
   const listDiv = document.createElement("div");
   listDiv.className = "list-div";
   const title = document.createElement("h3");
@@ -74,19 +76,21 @@ function addToList(todo) {
 
   deleteListButton.addEventListener("click", () => {
     list.removeTodoList(todo);
-    refreshList();
+    removeList();
+    refreshList(list, div);
   });
 
   title.textContent = todo.getTitle();
   description.textContent = todo.getDescription();
   date.textContent = todo.getDate();
+  console.log(todo.getDate());
   priority.textContent = todo.getPriority();
   listDiv.appendChild(title);
   listDiv.appendChild(description);
   listDiv.appendChild(date);
   listDiv.appendChild(priority);
   listDiv.appendChild(deleteListButton);
-  dom.appendChild(listDiv);
+  div.appendChild(listDiv);
 }
 
 const addButton = document.getElementById("add-todo-button");
@@ -94,7 +98,8 @@ const addButton = document.getElementById("add-todo-button");
 addButton.addEventListener("click", () => {
   list.addTodoList(createToDo());
   form.style.display = "none";
-  refreshList();
+  removeList();
+  refreshList(list, dom);
 });
 
 const newTaskButton = document.getElementById("new-task-button");
@@ -102,4 +107,57 @@ const form = document.getElementById("form-section");
 
 newTaskButton.addEventListener("click", () => {
   form.style.display = "block";
+});
+
+function closeMenu() {
+  const menuButtons = document.querySelectorAll(".menu-container");
+  menuButtons.forEach((button) => (button.style.display = "none"));
+}
+
+const homeButton = document.getElementById("home-button");
+const homeContainer = document.getElementById("home-container");
+homeButton.addEventListener("click", () => {
+  closeMenu();
+  homeContainer.style.display = "block";
+});
+
+const todayButton = document.getElementById("today-button");
+const todayContainer = document.getElementById("today-container");
+todayButton.addEventListener("click", () => {
+  closeMenu();
+  addTodayList();
+  todayContainer.style.display = "block";
+});
+
+const todayDate = format(new Date(), "yyyy-MM-dd");
+const todayList = document.getElementById("today-lists-content");
+
+function addTodayList() {
+  const filteredToday = list
+    .get()
+    .filter((list) => list.getDate() == todayDate);
+  filteredToday.forEach((day) => {
+    listToday.addTodoList(day);
+  });
+  refreshList(listToday, todayList);
+}
+
+const weekList = document.getElementById("week-lists-content");
+
+function addWeekList() {
+  const filteredWeek = list
+    .get()
+    .filter((list) => isThisWeek(parseISO(list.getDate())));
+  filteredWeek.forEach((day) => {
+    listWeek.addTodoList(day);
+  });
+  refreshList(listWeek, weekList);
+}
+
+const weekButton = document.getElementById("week-button");
+const weekContainer = document.getElementById("week-container");
+weekButton.addEventListener("click", () => {
+  closeMenu();
+  addWeekList();
+  weekContainer.style.display = "block";
 });
